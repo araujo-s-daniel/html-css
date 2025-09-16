32be826693cb43cb203427787a1c33c277296fee
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-09-15 21:07:58 -0300

    Cap. 07 - Melhorando os seus formulários

---
+++ placeholder/index.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {
+  function supportsPlaceholder() {
+    return (
+      "placeholder" in document.createElement("input") &&
+      "placeholder" in document.createElement("textarea")
+    );
+  }
+
+  if (!supportsPlaceholder()) {
+    $("html").addClass("no-placeholder");
+  }
+});
